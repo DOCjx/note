c5c1c076a5f81ed195c4df066ab898c799d14b9a
DOCDOCde <1324121459@qq.com>
2017-07-07 16:41:24 +0800

    使用jsonp跨域
    js跨域从数据库取数据

--- js/ajax/7.6.3.php
+++ js/ajax/7.6.3.php
@@ -1,25 +1,36 @@
 <?php
+	$name = @$_POST['name'] ? @$_POST['name'] : @$_GET['name'];
+	$action = @$_GET['action'];
 	header("Content-Type: text/plain;charset=utf-8"); 
 	$dsn = "mysql:host=localhost;dbname=bbb";
 	$db = new PDO($dsn,'root','');
 	$db->query('set names utf8');
-	$name = $_POST['name'];
 	// var_dump ($_POST);
-	$sql = "select * from student where name='$name'";
+	$sql = "select * from student where name ='$name'";
 	$result = $db->query($sql);
 	// var_dump($db);
 	$result->setFetchMode(PDO::FETCH_ASSOC);
-	$row = $result->fetch(1);
-	echo <<<STR
-	<table>
-		<tr>
-			<th>姓名：</th>
-			<th>学号：</th>
-		</tr>
-		<tr>
-			<td>$row[1]</td>
-			<td>$row[0]</td>
-		</tr>
-	</table>
+	$row = $result->fetch(4);
+	if( !$action ){
+		if( $row ){
+			echo <<<STR
+			<table>
+				<tr>
+					<th>姓名：</th>
+					<th>学号：</th>
+				</tr>
+				<tr>
+					<td>$row[1]</td>
+					<td>$row[0]</td>
+				</tr>
+			</table>
 STR;
+		}else{
+			echo '未找到结果!';
+		}
+	}else{
+		$getData = $_GET['callback'];
+		echo $getData.'('.json_encode($row).')';
+	}
+	
 ?>
--- js/ajax/aJax.php
+++ js/ajax/aJax.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head id="head">
 	<meta charset="UTF-8">
 	<title>ajax</title>
 	<style type="text/css">
@@ -9,31 +9,29 @@
 		}
 	</style>
 </head>
-<body>
+<body id="body">
 	<input type="text" id="keyword">
 	<button id="seach">查找</button>
 	<div id="result"></div>
 	<script>
-		//跨域问题解决办法
-		//http://www.cnblogs.com/2050/p/3191744.html
 		((w) => {
-			const getResult = () => {
+			const getResultXHR = () => {
 				const request = new XMLHttpRequest();
 				
 				// console.log(keyword);
 				// GET方法
-				// var keyword = "./server/7.6.3.php?name="+ document.getElementById("keyword").value;
+				// var keyword = "./7.6.3.php?name="+ document.getElementById("keyword").value;
 				// request.open("GET",keyword);
 				// request.send();
 				
 				//request方法
 				const data = "name="+document.getElementById("keyword").value;
-				request.open("POST","./server/7.6.3.php");
+				request.open("POST","./7.6.3.php");//http://127.0.0.1/note/js/ajax
 				request.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
 				request.send(data);
 				
 				request.onreadystatechange = () => {
-					console.log(request.responseText);
+					// 	console.log(request.responseText);
 					if( request.readyState===4 ){
 						if( request.status===200 ){
 							document.getElementById("result").innerHTML = request.responseText;
@@ -43,7 +41,63 @@
 					}
 				}
 			}
-			document.getElementById("seach").onclick = getResult;
+			const creatScript = (() => {
+				let _instance = null;
+				const creatTag = () => {
+					return {
+						el: "body",
+						src: window.location.href,
+						creatTagEl: function() {
+							const cb = 
+							"function getdata(res){let Id=res.ID;let Name=res.Name;"
+							+"		let htmlTpl = "
+							+"		'<table>'"
+							+"		+'	<tr>'"
+							+"		+'		<th>姓名：</th>'"
+							+"		+'		<th>学号：</th>'"
+							+"		+'	</tr>'"
+							+"		+'	<tr>'"
+							+"		+'		<td>'+Id+'</td>'"
+							+"		+'		<td>'+Name+'</td>'"
+							+"		+'	</tr>'"
+							+"		+'</table>';"
+							+"		if(!res) {"
+							+"      	htmlTpl = '未查询到数据！';"
+							+"      }"
+							+"		document.getElementById('result').innerHTML = htmlTpl;"
+							+"}";
+							const rootDom = document.getElementById(this.el);
+							const newScript = document.createElement('script');
+							newScript.type = 'text/javascript';
+							newScript.innerHTML = cb;
+							rootDom.appendChild(newScript);
+						},
+						getDataJsonP: function() {
+							const rootDom = document.getElementById(this.el);
+							const newScript = document.createElement('script');
+							const name = document.getElementById("keyword").value;
+							newScript.src = this.src + '?callback=getdata&action=jsonp&name='+name;
+							rootDom.appendChild(newScript);
+							const scriptTags = document.getElementsByTagName('script');
+							rootDom.removeChild(scriptTags[scriptTags.length-1]);
+						}
+					}
+					
+				}
+				return () => {
+					if( !_instance ){
+						_instance = creatTag();
+					}
+					return _instance;
+				}
+			})();
+			//向页面添加script和功能函数
+			// creatScript().el = 'head';//可以覆盖默认配置
+			creatScript().creatTagEl();
+			document.getElementById("seach").onclick = () => {
+				creatScript().src = "http://127.0.0.1/note/js/ajax/7.6.3.php";//可以覆盖默认配置
+				creatScript().getDataJsonP();
+			}
 		})(window);
 	</script>
 </body>
